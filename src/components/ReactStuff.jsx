1b27fc7d485c23b309ffdde5deb1a075b8de7412
hchiam <hchiam@users.noreply.github.com>
2021-07-18 10:30:48 -0400

    s+ svelte + react jsx

---
+++ src/components/ReactStuff.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+export function ReactStuff() {
+  return (
+    <section>
+      <h2>React stuff</h2>
+      <p>This stuff comes from a <code>.jsx</code> file!</p>
+    </section>
+  )
+}
---
+++ src/components/SvelteStuff.svelte
@@ -0,0 +1,4 @@
+<section>
+  <h2>Svelte stuff</h2>
+  <p>This stuff comes from a <code>.svelte</code> file!</p>
+</section>
